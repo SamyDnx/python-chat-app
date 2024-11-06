e476a08a3171a93843e0fdc2aa4088ab3f589730
SamyDnx <doneux.samy.uni@gmail.com>
2024-11-06 22:18:20 +0100

    created first client script (not tested)

--- client.py
+++ client.py
@@ -1,1 +1,31 @@
 import socket
+import threading
+
+IP = "127.0.0.1"
+PORT = 42069
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((IP, PORT))
+
+def receive_message():
+    while True:
+        try:
+            message = client.recv(1024).decode()
+            if message:
+                print(message)
+        except:
+            print("disconnected from server")
+            client.close()
+            break
+
+def send_message():
+    while True:
+        message = input()
+        client.send(message.encode())
+
+
+# run receive_message in parrallel with send_message (main thread)
+receive_thread = threading.Thread(target=receive_message)
+receive_thread.start()
+
+send_message()
